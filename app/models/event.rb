f261ae23219d47304d84a2d23cc62de5bfa6f7be
show39 <acacia.show@gmail.com>
2017-11-05 11:44:05 +0900

    イベントバリデーションの追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,5 +9,23 @@ class Event < ApplicationRecord
   validates :organizer, presence: true
   validates :tel, presence: true
   validates :email, presence: true
+  validates :article, presence: true, length: {maximum: 2000}
+  validates :capacity, presence: true, numericality: {greater_than_equal_to: 0}
+  validate  :event_start_should_be_before_event_end
+  validate  :recruit_start_should_be_before_recruit_end
 
+  private
+  def event_start_should_be_before_event_end
+    return unless event_start && event_end
+    if event_start >= event_end
+      errors.add(:event_start, 'はイベント終了時間よりも前に設定してください')
+    end
+  end
+
+  def recruit_start_should_be_before_recruit_end
+    return unless recruit_start && recruit_end
+    if recruit_start >= recruit_end
+      errors.add(:recruit_start, 'は募集終了時間よりも前に設定してください')
+    end
+  end
 end
