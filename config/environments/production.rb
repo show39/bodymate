01f8b6f83447a7b22150f14458927244893ef332
show39 <acacia.show@gmail.com>
2017-12-27 21:07:24 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,8 @@ Rails.application.configure do
   config.action_mailer.smtu87p_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    
+    :user_name => ENV["EMAIL_HIDDEN"],
+    :password => ENV["PASSWORD_HIDDEN"],
     :authentication => :plain,
     :enable_starttls_auto => true
   }
