ce9ac1c0b24be7e0328cc2b05dde56855373be45
show39 <acacia.show@gmail.com>
2018-01-24 16:53:28 +0900

    ユーザーページ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     now = Time.now
-    @wanted_events = Event.where('user_id = ?', current_user.id).where('recruit_start <= ? and recruit_end >= ?', now, now).order('event_start DESC').limit(5)
-    @finished_events = Event.where('user_id = ?', current_user.id).where('event_end < ?', now).order('event_start DESC').limit(5)
-    @entry_tickets = Booking.where('user_id', current_user.id).order('created_at DESC').limit(5)
+    @wanted_events = Event.where('user_id = ?', @user.id).where('recruit_start <= ? and recruit_end >= ?', now, now).order('event_start DESC').limit(5)
+    @finished_events = Event.where('user_id = ?', @user.id).where('event_end < ?', now).order('event_start DESC').limit(5)
+    @entry_tickets = Booking.where('user_id = ?', @user.id).order('created_at DESC').limit(5)
   end
 end
