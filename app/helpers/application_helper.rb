2cd286a7ce77b5d5c121e047f916c39caae07f5e
show39 <acacia.show@gmail.com>
2018-01-27 02:21:46 +0900

    イベント詳細テキストを自動リンク化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,17 @@
 module ApplicationHelper
+
+  # テキスト内のURLをリンクにする
+  require "uri"
+
+  def text_url_to_link(text)
+    URI.extract(text, ['http', 'https']).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+      text.gsub!(url, sub_text)
+    end
+    return text
+  end
+
   # ページごとに完全なタイトルを返す
   def full_title(page_title='')
     base_title = "Bobymate - スポーツイベントを無料掲載・集客支援"
