0da86d26cb0dc32d7ae00fac51dbd460a8ebfe3b
show39 <acacia.show@gmail.com>
2018-03-18 15:44:57 +0900

    マイページN+1解決

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     now = Time.now
     @wanted_events = Event.where('user_id = ?', @user.id).where('recruit_start <= ? and recruit_end >= ?', now, now).where('del_flg = ?', false).order('event_start DESC').limit(10)
     @finished_events = Event.where('user_id = ?', @user.id).where('event_end < ?', now).where('del_flg = ?', false).order('event_start DESC').limit(10)
-    @entry_tickets = Booking.where('user_id = ?', @user.id).includes(:ticket).order('created_at DESC').limit(10)
+    @entry_tickets = Booking.where('user_id = ?', @user.id).includes(ticket: [:event]).order('created_at DESC').limit(10)
   end
 
   def event
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
 
   def ticket
     @user = User.find(params[:id])
-    @tickets = Booking.where('user_id = ?', @user.id).order("created_at desc").page(params[:page]).per(24)
+    @tickets = Booking.where('user_id = ?', @user.id).includes(ticket: [:event]).order("created_at desc").page(params[:page]).per(24)
   end
 end
