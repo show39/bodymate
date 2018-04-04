a21e2d22cd5412187f6aa5d65f05c47ce43264a4
show39 <acacia.show@gmail.com>
2018-04-04 10:46:36 +0900

    fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, 'log/crontab.log'
 set :environment, :production
 
-every day, :at => '12am', roles: %i(app) do
+every 1.day, :at => '12am', roles: %i(app) do
   rake '-s sitemap:refresh'
 end
